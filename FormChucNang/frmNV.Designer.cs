8bf215c157c2ea1ecd30bd534ae68f1f4ea27be8
truong2163 <167168499+truong2163@users.noreply.github.com>
2024-05-05 16:01:02 +0700

    Add files via upload

---
+++ FormChucNang/frmNV.Designer.cs
@@ -0,0 +1,277 @@
+﻿
+namespace Nhom11_Quanlybangiay.FormChucNang
+{
+    partial class frmNV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnDangXuat = new System.Windows.Forms.Button();
+            this.btnDoiMatKhau = new System.Windows.Forms.Button();
+            this.btnXemSanPhamTonKho = new System.Windows.Forms.Button();
+            this.btnHoaDonNhapDaTao = new System.Windows.Forms.Button();
+            this.btnXem = new System.Windows.Forms.Button();
+            this.btnNhapHang = new System.Windows.Forms.Button();
+            this.btnBanHang = new System.Windows.Forms.Button();
+            this.btnChucNang = new System.Windows.Forms.Button();
+            this.pnl_hienthi = new System.Windows.Forms.Panel();
+            this.txtwelcome = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.BackColor = System.Drawing.SystemColors.Info;
+            this.panel2.Controls.Add(this.btnDangXuat);
+            this.panel2.Controls.Add(this.btnDoiMatKhau);
+            this.panel2.Controls.Add(this.btnXemSanPhamTonKho);
+            this.panel2.Controls.Add(this.btnHoaDonNhapDaTao);
+            this.panel2.Controls.Add(this.btnXem);
+            this.panel2.Controls.Add(this.btnNhapHang);
+            this.panel2.Controls.Add(this.btnBanHang);
+            this.panel2.Controls.Add(this.btnChucNang);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 127);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(289, 622);
+            this.panel2.TabIndex = 3;
+            // 
+            // btnDangXuat
+            // 
+            this.btnDangXuat.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDangXuat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDangXuat.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangXuat.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnDangXuat.Image = global::Nhom11_Quanlybangiay.Properties.Resources.Logout1;
+            this.btnDangXuat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDangXuat.Location = new System.Drawing.Point(0, 395);
+            this.btnDangXuat.Name = "btnDangXuat";
+            this.btnDangXuat.Size = new System.Drawing.Size(289, 47);
+            this.btnDangXuat.TabIndex = 8;
+            this.btnDangXuat.Text = "Đăng xuất";
+            this.btnDangXuat.UseVisualStyleBackColor = true;
+            this.btnDangXuat.Click += new System.EventHandler(this.btnDangXuat_Click);
+            // 
+            // btnDoiMatKhau
+            // 
+            this.btnDoiMatKhau.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDoiMatKhau.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDoiMatKhau.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDoiMatKhau.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnDoiMatKhau.Image = global::Nhom11_Quanlybangiay.Properties.Resources.changepassword;
+            this.btnDoiMatKhau.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDoiMatKhau.Location = new System.Drawing.Point(0, 336);
+            this.btnDoiMatKhau.Name = "btnDoiMatKhau";
+            this.btnDoiMatKhau.Size = new System.Drawing.Size(289, 59);
+            this.btnDoiMatKhau.TabIndex = 7;
+            this.btnDoiMatKhau.Text = "Đổi mật khẩu";
+            this.btnDoiMatKhau.UseVisualStyleBackColor = true;
+            this.btnDoiMatKhau.Click += new System.EventHandler(this.btnDoiMatKhau_Click);
+            // 
+            // btnXemSanPhamTonKho
+            // 
+            this.btnXemSanPhamTonKho.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnXemSanPhamTonKho.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnXemSanPhamTonKho.Font = new System.Drawing.Font("Times New Roman", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXemSanPhamTonKho.ForeColor = System.Drawing.Color.Red;
+            this.btnXemSanPhamTonKho.Location = new System.Drawing.Point(0, 279);
+            this.btnXemSanPhamTonKho.Name = "btnXemSanPhamTonKho";
+            this.btnXemSanPhamTonKho.Size = new System.Drawing.Size(289, 57);
+            this.btnXemSanPhamTonKho.TabIndex = 6;
+            this.btnXemSanPhamTonKho.Text = "* Sản phẩm tồn kho";
+            this.btnXemSanPhamTonKho.UseVisualStyleBackColor = true;
+            this.btnXemSanPhamTonKho.Visible = false;
+            this.btnXemSanPhamTonKho.Click += new System.EventHandler(this.btnXemSanPhamTonKho_Click);
+            // 
+            // btnHoaDonNhapDaTao
+            // 
+            this.btnHoaDonNhapDaTao.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnHoaDonNhapDaTao.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHoaDonNhapDaTao.Font = new System.Drawing.Font("Times New Roman", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHoaDonNhapDaTao.ForeColor = System.Drawing.Color.Red;
+            this.btnHoaDonNhapDaTao.Location = new System.Drawing.Point(0, 222);
+            this.btnHoaDonNhapDaTao.Name = "btnHoaDonNhapDaTao";
+            this.btnHoaDonNhapDaTao.Size = new System.Drawing.Size(289, 57);
+            this.btnHoaDonNhapDaTao.TabIndex = 5;
+            this.btnHoaDonNhapDaTao.Text = "* Hóa đơn nhập đã tạo";
+            this.btnHoaDonNhapDaTao.UseVisualStyleBackColor = true;
+            this.btnHoaDonNhapDaTao.Visible = false;
+            this.btnHoaDonNhapDaTao.Click += new System.EventHandler(this.btnHoaDonNhapDaTao_Click);
+            // 
+            // btnXem
+            // 
+            this.btnXem.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnXem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnXem.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXem.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnXem.Image = global::Nhom11_Quanlybangiay.Properties.Resources.report__1_1;
+            this.btnXem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnXem.Location = new System.Drawing.Point(0, 166);
+            this.btnXem.Name = "btnXem";
+            this.btnXem.Size = new System.Drawing.Size(289, 56);
+            this.btnXem.TabIndex = 3;
+            this.btnXem.Text = "Xem";
+            this.btnXem.UseVisualStyleBackColor = true;
+            this.btnXem.Click += new System.EventHandler(this.btnXem_Click);
+            // 
+            // btnNhapHang
+            // 
+            this.btnNhapHang.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnNhapHang.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNhapHang.Font = new System.Drawing.Font("Times New Roman", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNhapHang.ForeColor = System.Drawing.Color.Red;
+            this.btnNhapHang.Location = new System.Drawing.Point(0, 109);
+            this.btnNhapHang.Name = "btnNhapHang";
+            this.btnNhapHang.Size = new System.Drawing.Size(289, 57);
+            this.btnNhapHang.TabIndex = 2;
+            this.btnNhapHang.Text = "* Nhập hàng";
+            this.btnNhapHang.UseVisualStyleBackColor = true;
+            this.btnNhapHang.Visible = false;
+            this.btnNhapHang.Click += new System.EventHandler(this.btnNhapHang_Click);
+            // 
+            // btnBanHang
+            // 
+            this.btnBanHang.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnBanHang.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBanHang.Font = new System.Drawing.Font("Times New Roman", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBanHang.ForeColor = System.Drawing.Color.Red;
+            this.btnBanHang.Location = new System.Drawing.Point(0, 52);
+            this.btnBanHang.Name = "btnBanHang";
+            this.btnBanHang.Size = new System.Drawing.Size(289, 57);
+            this.btnBanHang.TabIndex = 1;
+            this.btnBanHang.Text = "* Bán hàng";
+            this.btnBanHang.UseVisualStyleBackColor = true;
+            this.btnBanHang.Visible = false;
+            this.btnBanHang.Click += new System.EventHandler(this.btnBanHang_Click);
+            // 
+            // btnChucNang
+            // 
+            this.btnChucNang.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnChucNang.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChucNang.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChucNang.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnChucNang.Image = global::Nhom11_Quanlybangiay.Properties.Resources.Chức_năng;
+            this.btnChucNang.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnChucNang.Location = new System.Drawing.Point(0, 0);
+            this.btnChucNang.Name = "btnChucNang";
+            this.btnChucNang.Size = new System.Drawing.Size(289, 52);
+            this.btnChucNang.TabIndex = 0;
+            this.btnChucNang.Text = "Chức năng";
+            this.btnChucNang.UseVisualStyleBackColor = true;
+            this.btnChucNang.Click += new System.EventHandler(this.btnChucNang_Click);
+            // 
+            // pnl_hienthi
+            // 
+            this.pnl_hienthi.AutoScroll = true;
+            this.pnl_hienthi.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnl_hienthi.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_hienthi.Location = new System.Drawing.Point(289, 127);
+            this.pnl_hienthi.Name = "pnl_hienthi";
+            this.pnl_hienthi.Size = new System.Drawing.Size(807, 622);
+            this.pnl_hienthi.TabIndex = 4;
+            // 
+            // txtwelcome
+            // 
+            this.txtwelcome.AutoSize = true;
+            this.txtwelcome.Font = new System.Drawing.Font("Times New Roman", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtwelcome.ForeColor = System.Drawing.Color.White;
+            this.txtwelcome.Location = new System.Drawing.Point(354, 0);
+            this.txtwelcome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.txtwelcome.Name = "txtwelcome";
+            this.txtwelcome.Size = new System.Drawing.Size(202, 90);
+            this.txtwelcome.TabIndex = 0;
+            this.txtwelcome.Text = "hello";
+            this.txtwelcome.Click += new System.EventHandler(this.txtwelcome_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Purple;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtwelcome);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1096, 127);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(307, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Quyền: Quản lý";
+            // 
+            // frmNV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Beige;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1096, 749);
+            this.Controls.Add(this.pnl_hienthi);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmNV";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Nhân viên";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmNV_FormClosed);
+            this.Load += new System.EventHandler(this.frmNV_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnNhapHang;
+        private System.Windows.Forms.Button btnBanHang;
+        private System.Windows.Forms.Button btnXem;
+        private System.Windows.Forms.Button btnXemSanPhamTonKho;
+        private System.Windows.Forms.Button btnHoaDonNhapDaTao;
+        private System.Windows.Forms.Button btnDangXuat;
+        private System.Windows.Forms.Button btnDoiMatKhau;
+        private System.Windows.Forms.Button btnChucNang;
+        private System.Windows.Forms.Panel pnl_hienthi;
+        private System.Windows.Forms.Label txtwelcome;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
